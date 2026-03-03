57ade06c299c933b08b12ef402139c34002f664a
Evan <evan.chencheng1@gmail.com>
2026-03-03 17:14:30 -0600

    feat: apply instructions banner to the rest of the guides

--- app/levels/3/guide_3.tsx
+++ app/levels/3/guide_3.tsx
@@ -3,65 +3,83 @@ import { ImageBackground, StyleSheet, View, TouchableOpacity } from 'react-nativ
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import BackButton from '@/app/misc/BackButton';
 import NextButton from '@/app/misc/NextButton';
+import { READ_GUIDE_INSTRUCTIONS } from '@/app/misc/instructions'
+import InstructionsBanner from '@/app/screens/InstructionsBanner';
 import { NavigationProp } from '@react-navigation/native';
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 const Guide = ({ navigation }: { navigation: NavigationProp<any> }) => {
-    const bgImage = require('@/assets/images/guia3.png');
+  const bgImage = require('@/assets/images/guia3.png');
 
-    const [mode, setMode] = useState<'read' | 'listen' | null>(null);
+  const [mode, setMode] = useState<'read' | 'listen' | null>(null);
 
-    useEffect(() => {
-        const fetchMode = async () => {
-            const storedMode = await AsyncStorage.getItem('mode');
-            setMode(storedMode === 'read' || storedMode === 'listen' ? storedMode : 'listen');
-        };
-        fetchMode();
-    }, []);
+  useEffect(() => {
+      const fetchMode = async () => {
+          const storedMode = await AsyncStorage.getItem('mode');
+          setMode(storedMode === 'read' || storedMode === 'listen' ? storedMode : 'listen');
+      };
+      fetchMode();
+  }, []);
 
-    return (
-        <SafeAreaProvider>
-        <SafeAreaView style={styles.container}>
-          <ImageBackground
-            source={bgImage}
-            style={styles.bgImage}
-            imageStyle={{ resizeMode: 'contain' }}>
-            <View style={styles.buttonsBackContainer}>
-              <BackButton/>
-            </View>
-            <View style={styles.buttonsNextContainer}>
-                <NextButton navigation={navigation} nextName="Level3" />
-            </View>
-          </ImageBackground>
-        </SafeAreaView>
-      </SafeAreaProvider>
-    );
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        {/* Background Image */}
+        <ImageBackground
+          source={bgImage}
+          style={styles.bgImage}
+          imageStyle={{ resizeMode: 'contain' }}>
+        </ImageBackground>
+        
+        {/* Back Button */}
+        <View style={styles.buttonsBackContainer}>
+          <BackButton/>
+        </View>
+
+        {/* Instructions Banner */}
+        <View style={styles.instructionBannerContainer}>
+          <InstructionsBanner instructions={READ_GUIDE_INSTRUCTIONS} />
+        </View>
+
+        {/* Next Button */}
+        <View style={styles.buttonsNextContainer}>
+          <NextButton navigation={navigation} nextName="Level3" />
+        </View>
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
 };
 
 const styles: { [key: string]: any } = StyleSheet.create({
-    container: {
-        flex: 1,
-          alignItems: 'center',
-          justifyContent: 'center',
-          backgroundColor: '#ffff',
-      },
-      bgImage: {
-        flex: 1,
-        width: wp('80%'),
-        height: hp('90%'),
-      },
-      buttonsBackContainer: {
-        position: 'absolute',
-        top: hp('-3%'),
-        left: wp('-8%'),
-        resizeMode: 'cover',
-    },
-    buttonsNextContainer: {
-        position: 'absolute',
-        bottom: hp('-1%'),
-        right: wp('-6%'),
-    }
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#ffff',
+  },
+  bgImage: {
+    flex: 1,
+    width: wp('80%'),
+    height: hp('90%'),
+  },
+  buttonsBackContainer: {
+    position: 'absolute',
+    top: hp('-2%'),
+    left: wp('2%'),
+    resizeMode: 'cover',
+  },
+  instructionBannerContainer: {
+    position: 'absolute',
+    top: hp('11%'),
+    left: wp('2%'),
+    zIndex: 5,
+  },
+  buttonsNextContainer: {
+    position: 'absolute',
+    bottom: hp('0%'),
+    right: wp('2%'),
+  }
 });
 
 export default Guide;
--- app/levels/7/guide_7.tsx
+++ app/levels/7/guide_7.tsx
@@ -1,71 +1,85 @@
-import BackButton from '@/app/misc/BackButton';
-import NextButton from '@/app/misc/NextButton';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { NavigationProp } from '@react-navigation/native';
 import React, { useEffect, useState } from 'react';
 import { ImageBackground, Platform, StyleSheet, View } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { NavigationProp } from '@react-navigation/native';
+import BackButton from '@/app/misc/BackButton';
+import NextButton from '@/app/misc/NextButton';
+import { READ_GUIDE_INSTRUCTIONS } from '@/app/misc/instructions'
+import InstructionsBanner from '@/app/screens/InstructionsBanner';
 import { heightPercentageToDP as hp, widthPercentageToDP as wp } from 'react-native-responsive-screen';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 const Guide = ({ navigation }: { navigation: NavigationProp<any> }) => {
-    const bgImage = require('@/assets/images/guia7.png');
+  const bgImage = require('@/assets/images/guia7.png');
 
-    const [mode, setMode] = useState<'read' | 'listen' | null>(null);
+  const [mode, setMode] = useState<'read' | 'listen' | null>(null);
 
-    useEffect(() => {
-        const fetchMode = async () => {
-            const storedMode = await AsyncStorage.getItem('mode');
-            setMode(storedMode === 'read' || storedMode === 'listen' ? storedMode : 'listen');
-        };
-        fetchMode();
-    }, []);
+  useEffect(() => {
+      const fetchMode = async () => {
+          const storedMode = await AsyncStorage.getItem('mode');
+          setMode(storedMode === 'read' || storedMode === 'listen' ? storedMode : 'listen');
+      };
+      fetchMode();
+  }, []);
 
-    return (
-        <SafeAreaProvider>
-        <SafeAreaView style={styles.container}>
-          <ImageBackground
-            source={bgImage}
-            style={styles.bgImage}
-            imageStyle={{ resizeMode: 'contain' }}>
-            <View style={styles.buttonsBackContainer}>
-              <BackButton/>
-            </View>
-            <View style={styles.buttonsNextContainer}>
-                <NextButton navigation={navigation} nextName="Level7" />
-            </View>
-          </ImageBackground>
-        </SafeAreaView>
-      </SafeAreaProvider>
-    );
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        {/* Background Image */}
+        <ImageBackground
+          source={bgImage}
+          style={styles.bgImage}
+          imageStyle={{ resizeMode: 'contain' }}>
+        </ImageBackground>
+        
+        {/* Back Button */}
+        <View style={styles.buttonsBackContainer}>
+          <BackButton/>
+        </View>
+
+        {/* Instructions Banner */}
+        <View style={styles.instructionBannerContainer}>
+          <InstructionsBanner instructions={READ_GUIDE_INSTRUCTIONS} />
+        </View>
+
+        {/* Next Button */}
+        <View style={styles.buttonsNextContainer}>
+          <NextButton navigation={navigation} nextName="Level7" />
+        </View>
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
 };
 
 const styles: { [key: string]: any } = StyleSheet.create({
-    container: {
-        flex: 1,
-          alignItems: 'center',
-          justifyContent: 'center',
-          backgroundColor: '#ffff',
-      },
-      bgImage: Platform.OS === 'web' ? {
-        flex: 1,
-        width: wp('80%'),
-        height: hp('90%'),
-      } : {
-        flex: 1,
-        width: wp('80%'),
-        height: hp('90%'),
-      },
-      buttonsBackContainer: {
-        position: 'absolute',
-        top: hp('-3%'),
-        left: wp('-8%'),
-        resizeMode: 'cover',
-    },
-    buttonsNextContainer: {
-        position: 'absolute',
-        bottom: hp('-1%'),
-        right: wp('-6%'),
-    }
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#ffff',
+  },
+  bgImage: {
+    flex: 1,
+    width: wp('80%'),
+    height: hp('90%'),
+  },
+  buttonsBackContainer: {
+    position: 'absolute',
+    top: hp('-2%'),
+    left: wp('2%'),
+    resizeMode: 'cover',
+  },
+  instructionBannerContainer: {
+    position: 'absolute',
+    top: hp('11%'),
+    left: wp('2%'),
+    zIndex: 5,
+  },
+  buttonsNextContainer: {
+    position: 'absolute',
+    bottom: hp('0%'),
+    right: wp('2%'),
+  }
 });
 
 export default Guide;
