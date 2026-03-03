432f5490b623dd2f9aa18d02f9302f9a94e45ab7
Evan <evan.chencheng1@gmail.com>
2026-03-03 15:41:50 -0600

    feat: introduce initial version of instructions banner component

---
+++ app/screens/InstructionsBanner.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import HoverTooltip from '@/components/HoverTooltip';
+
+const InstructionsBanner = ({ instructions }: { instructions: string }) => {
+  return (
+    <HoverTooltip
+      tooltipStyle={styles.tooltipContainer}
+      explanation={instructions}
+    >
+      <View style={styles.bannerContainer}>
+        <Text style={styles.bannerText}>Instrucciones</Text>
+      </View>
+    </HoverTooltip>
+  );
+}
+
+const styles = StyleSheet.create({
+  tooltipContainer: {
+    top: hp('8%'),
+    left: 0,
+    width: 450,
+  },
+  bannerContainer: {
+    backgroundColor: '#FFFFFF',
+    paddingHorizontal: 10,
+    borderRadius: 8,
+    borderWidth: 3,
+    borderColor: '#FF8C00',
+  },
+  bannerText: {
+    color: '#FF8C00',
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  }
+});
+
+export default InstructionsBanner;
