be8eff0bd2477075cfcd1d30ee72318c1e27dd15
Evan <evan.chencheng1@gmail.com>
2026-03-03 23:11:42 -0600

    refactor: rephrase listen levels instructions

--- app/misc/instructions.ts
+++ app/misc/instructions.ts
@@ -1,4 +1,4 @@
 export const READ_GUIDE_INSTRUCTIONS = 'Estudie cada término antes de practicar';
 export const READ_LEVEL_INSTRUCTIONS = 'Empareje cada palabra con su imagen correspondiente';
 export const LISTEN_GUIDE_INSTRUCTIONS = 'Escuche cada pronunciación antes de practicar';
-export const LISTEN_LEVEL_INSTRUCTIONS = 'Empareje cada audio con su imagen correspondiente';
+export const LISTEN_LEVEL_INSTRUCTIONS = 'Identifique la caja apropiada para cada audio';
