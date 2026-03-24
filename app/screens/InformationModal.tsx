07ecbdb77303b2d5b581a9208e325e12a7c1f7af
Evan <evan.chencheng1@gmail.com>
2026-03-23 20:32:30 -0600

    refactor: add conditional rendering to title

--- app/screens/InformationModal.tsx
+++ app/screens/InformationModal.tsx
@@ -37,7 +37,7 @@ const InformationModal: React.FC<InformationModalProps> = ({
           contentContainerStyle={styles.scrollPadding} 
           showsVerticalScrollIndicator={false}
         >
-          <Text style={styles.title}>{title}</Text>
+          {title && <Text style={styles.title}>{title}</Text>}
 
           <RenderHtml
             contentWidth={width}
